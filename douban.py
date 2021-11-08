5b59bdf83e7b0677083ab3134f9b16778c8f3dfe
TIANYI HU <ceiling121388@aliyun.com>
2021-11-08 05:14:55 +0000

    完成 增加ip代理（已过期

---
+++ douban.py
@@ -0,0 +1,133 @@
+from requests_html import HTMLSession
+from openpyxl import workbook
+from lxml import etree
+import os,random,re
+# 构建对象  括号不能少会报错
+session = HTMLSession()
+
+USER_AGENT_LIST = [
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3451.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:57.0) Gecko/20100101 Firefox/57.0',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2999.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.70 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 OPR/31.0.1889.174',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.1.4322; MS-RTC LM 8; InfoPath.2; Tablet PC 2.0)',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 OPR/55.0.2994.61',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.814.0 Safari/535.1',
+    'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; ja-jp) AppleWebKit/418.9.1 (KHTML, like Gecko) Safari/419.3',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; Touch; MASMJS)',
+    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1041.0 Safari/535.21',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3451.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:57.0) Gecko/20100101 Firefox/57.0',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2999.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.70 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36 OPR/31.0.1889.174',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.1.4322; MS-RTC LM 8; InfoPath.2; Tablet PC 2.0)',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 OPR/55.0.2994.61',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.814.0 Safari/535.1',
+    'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; ja-jp) AppleWebKit/418.9.1 (KHTML, like Gecko) Safari/419.3',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0; Touch; MASMJS)',
+    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1041.0 Safari/535.21',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4093.3 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko; compatible; Swurl) Chrome/77.0.3865.120 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4086.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:75.0) Gecko/20100101 Firefox/75.0',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/91.0.146 Chrome/85.0.4183.146 Safari/537.36',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36 VivoBrowser/8.4.72.0 Chrome/62.0.3202.84',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.60',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0',
+    'Mozilla/5.0 (X11; CrOS x86_64 13505.63.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:68.0) Gecko/20100101 Firefox/68.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.400',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
+]
+
+tunnel = "tps242.kdlapi.com:15818"
+
+username = "t13633606247196"
+password = "ld5xvdqv"
+proxies = {
+    "http": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": tunnel},
+    "https": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": tunnel}
+}
+wb = workbook.Workbook()
+ws = wb.active
+ws.append(['当前页url', '标题', '标题url', '作者', '作者url', '回应', '回应时间', '内容', '内容图片url'])
+# 构建对象  括号不能少会报错
+
+os_path = os.getcwd() + '/豆瓣/'
+if not os.path.exists(os_path):
+    os.mkdir(os_path)
+
+def db():
+    for i in range(14,24):
+        url = f'https://www.douban.com/group/GNZ48/discussion?start={i*25}&type=elite'
+        headers = {
+            'user-agent': random.choice(USER_AGENT_LIST),
+            'Cookie':'bid = DhXrKbqa3Fg;douban - fav - remind = 1;__yadk_uid = CejJOKzMapB4oH4WvenHkTNkcDmrwEe9;ll = "118329";__gads = ID = 60306c26315b11da - 22e21274a6ca00ca: T = 1627921566:RT = 1627921566:S = ALNI_MaZSSa_DDyxAMrvAIZ5xqF3heb0EQ;__utmc = 30149280;__utmv = 30149280.23974;__utmz = 30149280.1633617400.148.32.utmcsr = baidu | utmccn = (organic) | utmcmd = organic;ct = y;push_doumail_num = 0;dbcl2 = "239740621:b559XqvQ0DQ";ck = _wRz;_pk_ref.100001.8cb4 = ["", "", 1636343637,"https://www.baidu.com/link?url=d0ccEEQEo4lNpNhBa3kMGO-72jllrg6u-tfApUyAd3R27KlCZ2EtZw_d7tOJM2ok&wd=&eqid=c24c546b0008bbb600000006615f05bd"];_pk_ses.100001.8cb4 = *;ap_v = 0, 6.0;push_noty_num = 0;__utma = 30149280.356891611.1627573649.1636337738.1636343638.164;__utmt = 1;_pk_id.100001.8cb4 = bf358952db2791d6.1627567291.164.1636343728.1636339117.;__utmb = 30149280.54.4.1636343727834'
+        }
+
+        ss = session.get(url,headers=headers,proxies=proxies).html
+        # 当前页链接
+        # 标题
+        bt = ss.xpath('//table[@class="olt"]/tr/td[@class="title"]/a/text()')
+        # 标题链接
+        url_lj = ss.xpath('//*[@id="content"]/div/div[1]/div/table/tr/td[1]/a/@href')
+        # 作者
+        zz = ss.xpath('//*[@id="content"]/div/div[1]/div/table/tr/td[2]/a/text()')
+        # 回应
+        hy = ss.xpath('//*[@id="content"]/div/div[1]/div/table/tr/td[3]/text()')[1:]
+        # 回应时间
+        hy_sj = ss.xpath('//*[@id="content"]/div/div[1]/div[2]/table/tr/td[4]/text()')[1:]
+        # 发帖人链接
+        ftr_url = ss.xpath('//*[@id="content"]/div/div[1]/div[2]/table/tr/td[2]/a/@href')
+        # print(len(bt))
+        # print(len(url_lj))
+        # print(len(zz))
+        # print(len(hy))
+        # print(len(hy_sj))
+        # print(len(ftr_url))
+        for bts, url_ljs,zzs,hys,hy_sjs,ftr_urls in zip(bt,url_lj,zz,hy,hy_sj,ftr_url):
+            bts = bts.replace('\n', '').strip()
+            # print('标题：',bts)
+            # print(url_ljs)
+            # print(zzs)
+            # print(hys)
+            # print(hy_sjs)
+            # print()
+            response = session.get(url_ljs,headers=headers,proxies=proxies).html
+            # 每个标题的内容
+            bt_nr = ' , '.join(response.xpath('//div[@class="rich-content topic-richtext"]//text()'))
+            # 每个标题的图片链接
+            tp_url = '  ==  '.join(response.xpath('//div[@class="rich-content topic-richtext"]//@src'))
+            # 这里可以改改分开，每次拿到就下载到本地，不过估计需要很大的存储空间。
+
+            # print(bt_nr)
+            # print(tp_url)
+            print(f'标题：{bts}采集完成')
+            list_1 = [url,bts,url_ljs,zzs,ftr_urls,hys,hy_sjs,bt_nr,tp_url]
+            ws.append(list_1)
+            wb.save(os_path + '豆瓣3.xlsx')
+        print(f"============第{i+1}页===========")
+
+
+if __name__ == '__main__':
+    db()
